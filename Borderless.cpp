a8db8074a2ed08669cd01334e08509fa966321cd
Fahad704 <shaikh.fahad.77377@gmail.com>
2025-09-03 18:24:48 +0530

    Commit before merge

--- Borderless.cpp
+++ Borderless.cpp
@@ -1,14 +1,14 @@
 #include <windows.h>
-#include <string>
+#include <psapi.h>
 #include <iostream>
-const std::string EXE_NAME = "Sekiro";
-int main(){
-    std::string command = "start "+EXE_NAME+".exe";
-    system(command.c_str());
-    Sleep(2000);
-    HWND windowHandle = FindWindowA(NULL,EXE_NAME.c_str());
+#include <string>
+BOOL EnumWindowsProc(HWND window,LPARAM lparam){
+    
+}
+int makeborderless(std::string& exeName){
+    HWND windowHandle = FindWindowA(NULL,exeName.c_str());
     if(!windowHandle){
-        std::cerr << "failed to find "+EXE_NAME+" handle\n";
+        std::cerr << "Failed to find window handle of "+exeName+"\n";
         return 1;
     }
     LONG style = GetWindowLong(windowHandle,GWL_STYLE);
@@ -19,5 +19,9 @@ int main(){
     exStyle &= ~(WS_EX_DLGMODALFRAME | WS_EX_CLIENTEDGE | WS_EX_STATICEDGE);
     SetWindowLong(windowHandle, GWL_EXSTYLE, exStyle);
     SetWindowPos(windowHandle,NULL,0,0,0,0,SWP_NOMOVE | SWP_NOSIZE | SWP_NOZORDER | SWP_FRAMECHANGED);
+    return 0;
+}
+int main() {
+    EnumWindows(EnumWindowsProc,0);
     return 0;
 }
